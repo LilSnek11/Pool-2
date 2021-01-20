15dd9816193d32cd45c65901a189b4636ffafe40
Szello Istvan <68303099+LilSnek11@users.noreply.github.com>
2021-01-20 07:05:35 -0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Fibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string c, d;
+            int n, f1 = 0, f2 = 1, aux=0;
+            c = Console.ReadLine();
+            n = Convert.ToInt32(c);
+            for(int i=3;i<=n;i++)
+            { aux = f2;
+                f2 = f2 + f1;
+                f1 = aux;
+            }
+            d = Convert.ToString(f2);
+            Console.WriteLine("Numarul este {0}", d);
+
+        }
+    }
+}
