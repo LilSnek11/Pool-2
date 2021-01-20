9f385d7157b2d61392de2fcd3affc24adbf618f5
Szello Istvan <68303099+LilSnek11@users.noreply.github.com>
2021-01-20 07:01:57 -0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Secventa_nr_pare
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string n,o;
+            int m, p, nr;
+            p = 0;
+            n = Console.ReadLine();
+            m = Convert.ToInt32(n);
+            for (int i = 1; i <= m; i++)
+            {
+                o = Console.ReadLine();
+                nr = Convert.ToInt32(o);
+                if (nr % 2 == 0)
+                    p++;
+            }
+                        n=Convert.ToString(p);
+            Console.Write("{0} sunt pare",n );
+
+        }
+    }
+}
