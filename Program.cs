8ce0968e7a1e2a64cd040f681843ec2c67ea6b65
Szello Istvan <68303099+LilSnek11@users.noreply.github.com>
2021-01-20 06:58:28 -0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Suma_si_prod_a_n_nr
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string c, s, p;
+            int n, suma, produs, i;
+            suma = 0;
+            produs = 1;
+            i = 1;
+            c = Console.ReadLine();
+            n = Convert.ToInt32(c);
+            while(i<=n)
+            {
+                
+                suma = suma + i;
+                produs = produs * i;
+                i++;
+            }
+            s = Convert.ToString(suma);
+            p = Convert.ToString(produs);
+            Console.WriteLine("Suma este {0}", s);
+            Console.WriteLine("Produsul este {0}", p);
+        }
+    }
+}
