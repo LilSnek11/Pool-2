26939f7c3134fb074375662584100229e1161727
Szello Istvan <68303099+LilSnek11@users.noreply.github.com>
2021-01-20 07:03:46 -0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace paranteze
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string n, l;
+            int p , d, c = 0, f = 0;
+            n=Console.ReadLine();
+            d = Convert.ToInt32(n);
+            p = d;
+
+            if (d == 1)
+                Console.WriteLine("Sirul este incorect lol.");
+            else
+             while(d==1 || d==0)
+            { n = Console.ReadLine();
+                d = Convert.ToInt32(n);
+                    if (d == p)
+                        c++;
+                    else if (f < c)
+                    {
+                        f = c;
+                        c = 0;
+                    }
+                    else c = 0;
+              if (d == 0 || d ==1 )
+                        p = d;
+                      
+
+              } 
+            if (p == 1)
+            {
+                l = Convert.ToString(f);
+                Console.WriteLine("Numarul maxim de incuibare este {0}", l );
+            }
+              else
+                Console.WriteLine("Sirul este incorect lol.");
+                       
+
+
+        }
+    }
+}
