6fdab18799b59b581136b132d1724aecf7a9239d
Szello Istvan <68303099+LilSnek11@users.noreply.github.com>
2021-01-20 07:02:45 -0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Poz_lui_a
+{
+    
+    class Program
+    {
+     
+
+        static void Main(string[] args)
+        {
+            string n, a, b, pz;
+            int nr, val_a, i = 0, poz, bg;
+            poz = 0;
+            Console.Write("Lungimea secventei=");
+            n = Console.ReadLine();
+            Console.Write("Numarul cautat=");
+            a = Console.ReadLine();
+            nr = Convert.ToInt32(n);
+            val_a = Convert.ToInt32(a);
+            while (i<nr)
+            {
+                b = Console.ReadLine();
+                bg = Convert.ToInt32(b);
+               if (bg == val_a)
+                    poz = i;
+              i++;
+            }if (poz == 0)
+                Console.WriteLine("-1");
+            else
+            {
+                pz = Convert.ToString(poz);
+                Console.WriteLine("Pozitia numarului cautat este {0}", pz);
+            }
+        }
+    }
+}
